fd333e702900529b635504a26ea02c43ed22ed7b
PedroRajao <pedrorajao@gmail.com>
2019-03-08 11:52:33 -0300

    Update config-model.php

--- config-model.php
+++ config-model.php
@@ -1,3 +1,3 @@
-// Fill with your account information, and rename this file to 'config.php'
+// Fill with your account information, and rename this to 'config.php'
 define('EMAIL', '{EMAIL}');
 define('PASS', '{PASSWORD}');
