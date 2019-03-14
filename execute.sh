5cf99aab411fd8b280a85bede161a1e1781e5235
PedroRajao <pedrorajao@gmail.com>
2019-03-14 16:20:57 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Adicionar um crontab para esse script rodar todo dia às 2:25
+# Adicionar um crontab para esse script rodar todo dia às HH:MM
 
 # Inicia chromedriver
 ./vendor/bin/chromedriver --url-base=/wd/hub &
 
-# Executa codecept auto_like 5 vezes
+# Executa codecept auto_like N vezes
 for i in {1..5}
 do
    ./vendor/bin/codecept run --steps
 done
 
 # Suspende pc até 5 minutos antes do horário que esse script rodou do dia seguinte
-./suspend_until.sh 02:20
+#./suspend_until.sh HH:MM (-5)
